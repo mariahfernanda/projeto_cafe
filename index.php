e5acfd7a8357e0e2be771ad71c95d0338bb386ba
MARIA FERNANDA BENTO DA SILVA <21276378@CFP127ALUNO.LOCAL>
2022-03-15 16:58:37 -0300

    15/03

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <div class="nomeRestaurante"> InOut</div>
+        <div class="nomeRestaurante2"> Coffee</div>
+        <div class="imagemLogo"><img src="./img/hearder/logo_cafeteria.png"></div>
+
+        <nav id="menu">
+        <ul>
+            <li> <a id="list" href="">Destaques </a>  </li>
+            <li> <a id="list" href="">Kopi Lowak</a>  </li>
+            <li> <a id="list" href="">Cafés     </a>  </li>
+            <li> <a id="list" href="">Promoção  </a>  </li>
+            <li> <a id="list" href="">Contatos  </a>  </li>
+        </ul>
+        </nav>
+    </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <main></main>
+    <footer></footer>
+</body>
+</html>
